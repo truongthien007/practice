eedc55c38307d1fedc81f07b4f8864ff9f0a2467
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-11 16:55:15 +0700

    sort the string

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,11 +1,18 @@
 """
 Word Occurrences
 Estimate: 40 minutes
-Actual:    minutes
+Actual:  55 minutes
 """
-word_to_occurrences = {}
+word_to_occurrence = {}
 sentence = input("Give us a string: ")
 words = sentence.split()
-print(words)
-for word in words:
-    if word in word_to_occur
+words.sort()
+for i in range(0, len(words)):
+    if words[i] not in word_to_occurrence:
+        # word_to_occurrence[words[i]] is value
+        word_to_occurrence[words[i]] = 1
+    else:
+        word_to_occurrence[words[i]] += 1
+
+print(word_to_occurrence)
+
