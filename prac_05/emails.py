7cf6070160de98cb0c14f3004b3a12071e337811
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-11 18:05:52 +0700

    complete task

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -8,12 +8,15 @@ def main():
         validate_name(email)
         print(email_to_name)
         email = input("Email: ")
+    for k, v in email_to_name.items():
+        print(f'{v} ({k})')
+
 
 
 def validate_name(email):
     request = input(f"Is your name {get_name(email)}? (y/n)")
     if request.lower() in ["n", "no"]:
-        correct_name = input("Name: ")
+        correct_name = input("Name: ").title()
         email_to_name[email] = correct_name
     elif request.lower() in ["y", "yes"]:
         email_to_name[email] = get_name(email)
