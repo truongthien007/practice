2653329c67793071ad897482201c4f2730101af0
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-11 15:31:03 +0700

    add EAFP method

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -6,9 +6,9 @@ File needs reformatting
 
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-# print(CODE_TO_NAME)
-#
-# state_code = input("Enter short state: ").upper()
+print(CODE_TO_NAME)
+
+state_code = input("Enter short state: ").upper()
 # while state_code != "":
 #     if state_code in CODE_TO_NAME:
 #         print(state_code, "is", CODE_TO_NAME[state_code])
@@ -16,6 +16,15 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern T
 #         print("Invalid short state")
 #     state_code = input("Enter short state: ").upper()
 
-for k, v in CODE_TO_NAME.items():
-    # k is key, v is value in dictionaries
-    print(f'{k: <3} is {v}')
+# for k, v in CODE_TO_NAME.items():
+# k is key, v is value in dictionaries
+# print(f'{k: <3} is {v}')
+
+# use EAFP
+while state_code != "":
+    try:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+        # state_code = input("Enter short state: ").upper()
+    except KeyError:
+        print("Error")
+    state_code = input("Enter short state: ").upper()
