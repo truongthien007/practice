963705e516f066c905f49b9a52933dfeb03f5ef3
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-18 15:51:20 +0700

    add str method and correct some minor error

---
+++ prac_06/programming_language.py
@@ -0,0 +1,21 @@
+"""
+estimate time:
+actually time:
+"""
+
+
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection=bool, year=0):
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.name = name
+
+    def is_dynamic(self):
+        if self.typing == "Static":
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection {self.reflection}, First appear in {self.year}"
