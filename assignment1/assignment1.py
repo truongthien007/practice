ccf84148b9fc068eb25e84ec9f77c110f79420c2
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-21 15:54:37 +0700

    print menu

---
+++ assignment1/assignment1.py
@@ -0,0 +1,14 @@
+"""
+Replace the contents of this module docstring with your own details
+Name: Thien Duong
+Date started: 19/12/2022
+GitHub URL: https://github.com/JCUS-CP1404/assignment-1-truongthien007
+"""
+
+menus = ["Menu:", "L - List all book", "A - Add new book", "M - Mark a book as completed", "Q - Quit"]
+
+print("Reading Tracker 1.0 - by Thien Duong")
+print("4 books loaded")
+for menu in menus:
+    print(menu)
+
