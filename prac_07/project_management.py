cf894466ee29c3dc479efbe2778adbebf4f7a9d7
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-26 13:48:52 +0700

    extract data function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,4 +7,11 @@ for menu in MENU:
 def main():
     with open("projects.txt", "r", encoding="utf-8-sig") as in_file:
         data = in_file.readlines()
+        extract_data(data)
 
+def extract_data(data):
+    def extract_data(data):
+        projects = []
+        for line in data[1:]:
+            items = line.strip().split("\t")
+            projects.append(items)
