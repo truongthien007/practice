71e5a5b13b8df0017a85a859578de82bdc70136f
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-26 14:02:51 +0700

    add function is complete

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,3 +1,4 @@
+
 class Project:
     def __init__(self, name, start_date, priority=int, cost_estimate=float, completion_percentage=int):
         self.name = name
@@ -9,8 +10,10 @@ class Project:
     def __str__(self):
         return f"{self.name}, start:{self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, " \
                f"completion: {self.completion_percentage}% "
-   def is_not_completed(self):
+
+    def is_complete(self):
         if self.completion_percentage == 100:
-            return False
-        else:
             return True
+        else:
+            return False
+
