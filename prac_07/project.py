92351b66586b3874dbbc7434437588758e438418
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-26 13:47:19 +0700

    create class project

---
+++ prac_07/project.py
@@ -0,0 +1,11 @@
+class Project:
+    def __init__(self, name, start_date, priority=int, cost_estimate=float, completion_percentage=int):
+        self.name = name
+        self.start_date = start_date
+        self.priority = priority
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def __str__(self):
+        return f"{self.name}, start:{self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}, " \
+               f"completion: {self.completion_percentage}% "
