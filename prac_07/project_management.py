752b203a0e57968cb73a8febe36b4f25dc875d85
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-26 13:52:15 +0700

    add get choice function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,17 +1,26 @@
 MENU = ['-(L)oad project', "-(S)ave project", "-(D)isplay project", "-(F)ilter project by date", "-(A)dd new project",
         "-(U)pdate project"]
-for menu in MENU:
-    print(menu)
 
 
 def main():
-    with open("projects.txt", "r", encoding="utf-8-sig") as in_file:
+    with open("project.txt", "r", encoding="utf-8-sig") as in_file:
         data = in_file.readlines()
         extract_data(data)
+    choice = get_choice()
+
+
+def get_choice():
+    for menu in MENU:
+        print(menu)
+    choice = input(">>>").upper()
+    return choice
+
 
 def extract_data(data):
-    def extract_data(data):
-        projects = []
-        for line in data[1:]:
-            items = line.strip().split("\t")
-            projects.append(items)
+    projects = []
+    for line in data[1:]:
+        items = line.strip().split("\t")
+        projects.append(items)
+
+
+main()
