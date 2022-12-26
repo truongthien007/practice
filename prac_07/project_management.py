6522e05373b4977690fe685f1fdc508c08bd10a4
truongthien007 <huynhtruongthien.duong@my.jcu.edu.au>
2022-12-26 13:47:53 +0700

    create main function and read file

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,4 +1,10 @@
-MENU = ['(L)oad project', "(S)ave project", "(D)isplay project", "(F)ilter project by date", "(A)dd new project",
-        "(U)pdate project"]
+MENU = ['-(L)oad project', "-(S)ave project", "-(D)isplay project", "-(F)ilter project by date", "-(A)dd new project",
+        "-(U)pdate project"]
 for menu in MENU:
     print(menu)
+
+
+def main():
+    with open("projects.txt", "r", encoding="utf-8-sig") as in_file:
+        data = in_file.readlines()
+
